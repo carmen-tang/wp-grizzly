a87bf2eaf824e1c9261d041026058ef03e64e72b
Carmen Tang <carmen.tang@thecontrolgroup.com>
2017-04-19 08:57:34 -0700

    changed padding for page & post

--- src/js/header.js
+++ src/js/header.js
@@ -81,7 +81,7 @@ d.slice(e-c+1,e+c+2).addClass("slick-active").attr("aria-hidden","false")),0===a
 
   	// sliderInit();
 
-  	console.log('Initalized'); 
+  	// console.log('Initalized'); 
 
 
   	// ==========================================================================
